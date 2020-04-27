b20700e466a031601b37dccfec5044e87575c969
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-04-27 23:28:31 +0530

    menu scrollabel

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -4,7 +4,7 @@ export default class AppBar extends React.Component {
   render(){
     return(
       <div className="header">
-        <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed pure-menu-scrollable">
             <a className="pure-menu-heading" href=""><img alt="Logo" src="logo.png" height="18" width="18"/>&nbsp;<strong>Project Kisaan</strong> - Team EDGE</a>
             <ul className="pure-menu-list">
                 <li className="pure-menu-item pure-menu-selected"><a href="#" className="pure-menu-link">Home</a></li>
