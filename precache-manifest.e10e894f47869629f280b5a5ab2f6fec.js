b59da3349a1375b444287aa1c21e12d4c065633d
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-04-27 23:10:33 +0530

    Updates

---
+++ precache-manifest.e10e894f47869629f280b5a5ab2f6fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152ff315c4159f1fe7bd8194ac9d8615",
+    "url": "/project-kisaan-react-ui/index.html"
+  },
+  {
+    "revision": "15b943564beb2fd1334b",
+    "url": "/project-kisaan-react-ui/static/css/2.4cca55f4.chunk.css"
+  },
+  {
+    "revision": "caa52ba2fc74c7cb931b",
+    "url": "/project-kisaan-react-ui/static/css/main.86e484ae.chunk.css"
+  },
+  {
+    "revision": "15b943564beb2fd1334b",
+    "url": "/project-kisaan-react-ui/static/js/2.acfc26a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-kisaan-react-ui/static/js/2.acfc26a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa52ba2fc74c7cb931b",
+    "url": "/project-kisaan-react-ui/static/js/main.c33be31e.chunk.js"
+  },
+  {
+    "revision": "dee6658333ee16dde7ff",
+    "url": "/project-kisaan-react-ui/static/js/runtime-main.8ab7ef9b.js"
+  }
+]);
