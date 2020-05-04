dee4e60e853c602b3cc233f7c4f7a0e4992e0601
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-04 13:12:14 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-kisaan-react-ui/precache-manifest.f124ce7a11ce4228eff1028a146e044e.js"
+  "/project-kisaan-react-ui/precache-manifest.3d1ec7c86b7f5be522fd9118e5c64347.js"
 );
 
 self.addEventListener('message', (event) => {
